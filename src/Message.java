21ce84c766b2808b6fede51e04c08713ad92581f
AntonAlexandrov <antond_aleksandrov@abv.bg>
2015-01-31 17:16:18 +0200

    Updated the main class, added a menu class and a menu

--- src/Message.java
+++ src/Message.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Message {
     public void printMessage(){
         System.out.println("Hello, this is an example maze!");
